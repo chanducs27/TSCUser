e381fda9afb4f38d16fb7bc0969cb91930395b1c
a <chanducs27>
2017-04-17 20:30:40 +0530

    added picuploc

--- app/src/main/java/com/fantasik/tscuser/tscuser/MapsActivity.java
+++ app/src/main/java/com/fantasik/tscuser/tscuser/MapsActivity.java
@@ -8,14 +8,14 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-
-
 import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
-
+import com.fantasik.tscuser.tscuser.Util.ResizeWidthAnimation;
+import com.fantasik.tscuser.tscuser.Util.Utils;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
@@ -23,23 +23,33 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
 import static android.content.Context.LOCATION_SERVICE;
 
 public class MapsActivity extends Fragment implements OnMapReadyCallback, LocationListener {
 
     GoogleMap googleMap;
+    @BindView(R.id.startlocpopup)
+    RelativeLayout startlocpopup;
+    Unbinder unbinder;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        return inflater.inflate(R.layout.fragment_maps, container, false);
+        View view = inflater.inflate(R.layout.fragment_maps, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        return view;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         //show error dialog if GoolglePlayServices not available
-
+        startlocpopup.setVisibility(View.GONE);
         //setContentView(R.layout.content_user_map);
         MapFragment fragment = (MapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         fragment.getMapAsync(this);
@@ -56,6 +66,12 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
 
+      //  startlocpopup.setVisibility(View.VISIBLE);
+
+        ResizeWidthAnimation anim = new ResizeWidthAnimation(startlocpopup, Math.round(Utils.convertDpToPixel(130)));
+        anim.setDuration(500);
+        startlocpopup.startAnimation(anim);
+        startlocpopup.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -78,15 +94,14 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
     public void onMapReady(GoogleMap gogleMap) {
         googleMap = gogleMap;
 
-        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+        if (ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getActivity(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION},
+                    200);
         }
         googleMap.setMyLocationEnabled(true);
         LocationManager locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
@@ -101,4 +116,13 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback, Locati
     }
 
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
+    }
+
+    @OnClick(R.id.startlocpopup)
+    public void onViewClicked() {
+    }
 }
---
+++ app/src/main/java/com/fantasik/tscuser/tscuser/Util/Utils.java
@@ -0,0 +1,33 @@
+package com.fantasik.tscuser.tscuser.Util;
+
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by a on 17-Apr-17.
+ */
+
+public class Utils {
+
+   /* public static int dpToPx(int dp)
+    {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
+    public static int pxToDp(int px)
+    {
+        return (int) (px / Resources.getSystem().getDisplayMetrics().density);
+    }*/
+    public static float convertPixelsToDp(float px){
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float dp = px / (metrics.densityDpi / 160f);
+        return Math.round(dp);
+    }
+
+    public static float convertDpToPixel(float dp){
+        DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+        float px = dp * (metrics.densityDpi / 160f);
+        return Math.round(px);
+    }
+
+}
